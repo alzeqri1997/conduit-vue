e180875d47fea66a449ef09ca4b30de94b3dc057
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2022-01-06 07:20:46 +0300

    add VTag Component

---
+++ src/components/VTag.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-link
+    :to="{ name: 'home-tag', params: { tag: name } }"
+    :class="className"
+    v-text="name"
+  ></router-link>
+</template>
+
+<script>
+export default {
+  name: "RwvTag",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    className: {
+      type: String,
+      default: "tag-pill tag-default",
+    },
+  },
+  mounted() {},
+};
+</script>
+
+<style>
+</style>
