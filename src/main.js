fb12af1f800a23b7f578c4d57d075ea548232d87
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2022-01-06 06:15:39 +0300

    add DateFilter

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ApiService from "./common/api.service";
+import DateFilter from "./common/date.filter";
 
 Vue.config.productionTip = false;
 ApiService.init();
+
+Vue.filter("date", DateFilter);
 
 new Vue({
   router,
