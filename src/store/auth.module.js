ba3eab0afe87a32967451d0899999a68a1655a4d
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2022-01-06 07:54:37 +0300

    add auth store

---
+++ src/store/auth.module.js
@@ -0,0 +1,107 @@
+import ApiService from "../common/api.service";
+import jwtService from "../common/jwt.service";
+import { LOGIN, LOGOUT, REGISTER, CHECK_AUTH, UPDATE_USER } from "./actions.type";
+
+import { SET_AUTH, PURGE_AUTH, SET_ERROR } from "./mutations.type";
+
+const state = {
+  errors: null,
+  user: {},
+  isAuthenticated: !!jwtService.getToken(),
+};
+
+const getters = {
+  currentUser(state) {
+    return state.user;
+  },
+  isAuthenticated(state) {
+    return state.isAuthenticated;
+  },
+};
+
+const actions = {
+  [LOGIN](context, credentials) {
+    return new Promise(resolve => {
+      ApiService.post("user/login", { user: credentials })
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+          resolve(data);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+        });
+    });
+  },
+  [LOGOUT](context) {
+    context.commit(PURGE_AUTH);
+  },
+  [REGISTER](context, credentials) {
+    return new Promise((resolve, reject) => {
+      ApiService.post("users", { user: credentials })
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+          resolve(data);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+          reject(response);
+        });
+    });
+  },
+  [CHECK_AUTH](context) {
+    if (jwtService.getToken()) {
+      ApiService.setHeader();
+      ApiService.get("user")
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+        });
+    } else {
+      context.commit(PURGE_AUTH);
+    }
+  },
+  [UPDATE_USER](context, payload) {
+    const { email, username, password, image, bio } = payload;
+    const user = {
+      email,
+      username,
+      bio,
+      image,
+    };
+    if (password) {
+      user.password = password;
+    }
+
+    return ApiService.put("user", user).then(({ data }) => {
+      context.commit(SET_AUTH, data.user);
+      return data;
+    });
+  },
+};
+
+const mutations = {
+  [SET_ERROR](state, error) {
+    state.errors = error;
+  },
+  [SET_AUTH](state, user) {
+    state.isAuthenticated = true;
+    state.user = user;
+    state.errors = {};
+    jwtService.saveToken(state.user.token);
+  },
+  [PURGE_AUTH](state) {
+    state.isAuthenticated = false;
+    state.user = {};
+    state.errors = {};
+    jwtService.destroyToken();
+  },
+};
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters,
+};
