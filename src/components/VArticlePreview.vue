d5be726f062e47ad7fc095ba880b76bc987ee685
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2022-01-06 07:20:26 +0300

    add ArticlePreview Component

---
+++ src/components/VArticlePreview.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="article-preview">
+    <RwvArticleMeta isPreview :article="article" />
+    <router-link :to="articleLink" class="preview-link">
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.description }}</p>
+      <span>Read more...</span>
+      <TagList :tags="article.tagList" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import RwvArticleMeta from "./ArticleMeta";
+import TagList from "./TagList";
+
+export default {
+  name: "RwvArticlePreview",
+  components: {
+    RwvArticleMeta,
+    TagList,
+  },
+  props: {
+    article: { type: Object, required: true },
+  },
+  computed: {
+    articleLink() {
+      return {
+        name: "article",
+        params: {
+          slug: this.article.slug,
+        },
+      };
+    },
+  },
+};
+</script>
+
+<style>
+</style>
